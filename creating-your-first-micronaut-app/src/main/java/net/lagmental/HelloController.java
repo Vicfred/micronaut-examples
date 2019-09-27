b3ac50f900c80b4129bf8c3a7166eb5ce5057994
Vicfred <vicfredboy@gmail.com>
2019-09-27 17:53:24 -0500

    olis controller

---
+++ creating-your-first-micronaut-app/src/main/java/net/lagmental/HelloController.java
@@ -0,0 +1,15 @@
+package net.lagmental;
+
+import io.micronaut.http.MediaType;
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Get;
+import io.micronaut.http.annotation.Produces;
+
+@Controller("/hello")
+public class HelloController {
+    @Get("/")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String index() {
+        return "Olis Olis";
+    }
+}
